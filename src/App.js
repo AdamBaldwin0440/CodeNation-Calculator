72b8f6e02a0594988e5a8a817a1c90ba3af19b3e
Adam Baldwin <adi.baldwin@hotmail.com>
2023-02-10 16:20:25 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const Calculator = () => {
+    const NumberKeys = [1,2,3,4,5,6,7,8,9,0];
+    const FuncKeys = ["+", "-", "*", "/", "="]
+    const [currentOp, setCurrentOp] = useState("")
+
+
+    let clickNumbers = (Val) =>{
+      Val === "=" ?
+      setCurrentOp(evaluate (currentOp))
+      :
+      setCurrentOp(currentOp + Val)
+    }
+
+    let clearOps = () => {
+    setCurrentOp(" ")
+    }
+
+    return (
+      <div className='App'>
+        <div className='CalculatorWrap'>
+          
+          <div className='leftKeys'>
+            <p id='Display'>{currentOp}</p> 
+            <button id = "Clear" onClick={clearOps}> Clear </button>
+            <div className='numbers'>
+              {NumberKeys.map((Val, index) => {
+                return <>
+                <button id='IndivNums' onClick={() => {clickNumbers(Val)}} key={index} > {Val} </button>
+                </>
+                
+              })
+              }
+            </div>
+          </div>
+          
+          <div className='rightKeys'></div>
+              <div className='Operations'>
+              {FuncKeys.map((Val, index) => {
+                return <button id="IndividualFuncKeys" onClick={() => {clickNumbers(Val)}} key={index}>  {Val}</button>
+              })}
+               
+              </div>
+              
+        </div>
+      </div>
+      
+    );
+  }
+
+export default Calculator;
